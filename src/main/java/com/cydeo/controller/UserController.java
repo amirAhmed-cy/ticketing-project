f544265347f3b92d40db253a3283d52a4b48ef74
amiritahmed <amiritahmed@gmail.com>
2023-10-17 00:32:38 -0400

    Part3

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -5,10 +5,7 @@ import com.cydeo.service.RoleService;
 import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/user")
@@ -47,6 +44,17 @@ public class UserController {
 
         userService.save(user);
         return "redirect:/user/create";  //user, roles, users
+    }
+
+    @GetMapping("/update/{username}")
+    public String editUser(@PathVariable("username") String username, Model model){
+
+        model.addAttribute("user", new UserDTO());
+        model.addAttribute("roles", roleService.findAll()); //bring me all roles form DB
+        model.addAttribute("users", userService.findAll());
+
+        return "user/update";
+
     }
 
 
